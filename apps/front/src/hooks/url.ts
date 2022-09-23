a0d12938aa92bf904118a8944cddc8d4d2812b48
hir0o <mk.9.q.kyu@gmail.com>
2022-09-23 11:44:24 +0900

    fix: serverDomain

--- apps/front/src/hooks/url.ts
+++ apps/front/src/hooks/url.ts
@@ -1,4 +1,4 @@
-const serverDomain = process.env.SERVER_URL || 'http://localhost:3000'
+const serverDomain = process.env.SERVER_URL || 'localhost:3000'
 export const httpUrl = 'http://' + serverDomain
 
 export const wsUrl = 'ws://' + serverDomain
