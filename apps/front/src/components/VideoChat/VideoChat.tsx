0f54e255901c0d84dff7c29acf9708f40cfc876f
hir0o <mk.9.q.kyu@gmail.com>
2022-09-30 09:15:31 +0900

    feat: 退室ボタンを作成

--- apps/front/src/components/VideoChat/VideoChat.tsx
+++ apps/front/src/components/VideoChat/VideoChat.tsx
@@ -1,5 +1,6 @@
 import { AspectRatio, Box, Container } from '@chakra-ui/react'
 import React, { FC, useEffect, useRef, useState } from 'react'
+import { useLeaveTheRoomConfirm } from '~/hooks/useLeaveTheRoomConfirm'
 import { useLinkStreamToVideoElm } from '~/hooks/useLinkStreamToVideoElm'
 import { useRTCConnection } from '~/hooks/useRTCConnection'
 import { useSocket } from '~/hooks/useSocket'
@@ -10,19 +11,20 @@ import { ButtonList } from '../ButtonList'
 type Props = {
   name: string
   stream: MediaStream | undefined
-  handleToggleMic: () => void
-  handleToggleCamera: () => void
   micOn: boolean
   cameraOn: boolean
+  handleToggleMic: () => void
+  handleToggleCamera: () => void
+  // handleLeaveTheRoom: () => void
 }
 
 export const VideoChat: FC<Props> = ({
   name,
   stream,
-  handleToggleMic,
-  handleToggleCamera,
   micOn,
   cameraOn,
+  handleToggleMic,
+  handleToggleCamera,
 }) => {
   const socket = useSocket()
   const localVideoRef = useRef<HTMLVideoElement>(null)
@@ -35,7 +37,8 @@ export const VideoChat: FC<Props> = ({
     remoteVideoWrapper: remoteVideoWrapperRef.current,
     name,
   })
-  console.log(connectionLength)
+
+  const { modal, open } = useLeaveTheRoomConfirm()
 
   return (
     <Container
@@ -45,28 +48,36 @@ export const VideoChat: FC<Props> = ({
           : 'container.xl'
       }
     >
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        flexWrap="wrap"
-        gap={4}
-        className="video-wrapper"
-        data-video-count={connectionLength}
-        ref={remoteVideoWrapperRef}
-      >
-        <div className="video">
-          <video data-name={name} ref={localVideoRef} autoPlay playsInline />
-          <span>{name}</span>
-        </div>
-      </Box>
-      <Box position="fixed" bottom={4} left="50%" transform="translateX(-50%)">
-        <ButtonList>
-          <VideoButton onClick={handleToggleCamera} isOn={cameraOn} />
-          <LeaveButton />
-          <MicButton onClick={handleToggleMic} isOn={micOn} />
-        </ButtonList>
-      </Box>
+      <>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexWrap="wrap"
+          gap={4}
+          className="video-wrapper"
+          data-video-count={connectionLength}
+          ref={remoteVideoWrapperRef}
+        >
+          <div className="video">
+            <video data-name={name} ref={localVideoRef} autoPlay playsInline />
+            <span>{name}</span>
+          </div>
+        </Box>
+        <Box
+          position="fixed"
+          bottom={4}
+          left="50%"
+          transform="translateX(-50%)"
+        >
+          <ButtonList>
+            <VideoButton onClick={handleToggleCamera} isOn={cameraOn} />
+            <LeaveButton onClick={open} />
+            <MicButton onClick={handleToggleMic} isOn={micOn} />
+          </ButtonList>
+        </Box>
+        {modal}
+      </>
     </Container>
   )
 }
