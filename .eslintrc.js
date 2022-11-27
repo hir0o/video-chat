358ee3f6c30737d5acbdec43022a83e6f60b3471
hir0o <mk.9.q.kyu@gmail.com>
2022-11-27 12:43:50 +0900

    モノレポやーめた

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,79 @@
 module.exports = {
-  root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/"],
-    },
+  env: {
+    browser: true,
+    es2021: true,
   },
-};
+  extends: [
+    'plugin:react/recommended',
+    'next',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'arrow-body-style': 0,
+    'no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true },
+    ],
+    'consistent-return': 0,
+    'default-case': 0,
+    'no-restricted-exports': 0,
+    'react/react-in-jsx-scope': 0,
+    'import/prefer-default-export': 0,
+    'react/prop-types': 0,
+    'no-void': 0,
+    // メディアのキャプションについての設定
+    'jsx-a11y/media-has-caption': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    // onClickなどに直接async functionを渡せるようにする
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+    'react/jsx-props-no-spreading': 0,
+  },
+  ignorePatterns: ['.eslintrc.*', '*.config.*'],
+}
